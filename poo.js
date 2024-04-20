e6f835e4a4d0e5dc61cd9f07e28bfb86e0ca3c86
RobertSouDev <rogerhallks1@gmail.com>
2024-04-19 22:13:00 -0300

    mais uma

---
+++ poo.js
@@ -0,0 +1,13 @@
+let aluno = function (){
+
+    const nome = "Italo"
+    const alunos = ["italo", "Vitor", "Roger", "Robert"]
+
+    function buscar(){
+        console.log(alunos[0])
+    }
+    buscar()
+
+}
+
+console.log(aluno())
